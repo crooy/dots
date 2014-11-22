2c4aeaf22df32263cf056755398c9a21a002bce1
Paul Rose <paul@paulrose.com>
2014-11-22 12:17:07 +0000

    Typo

--- os/index.sh
+++ os/index.sh
@@ -10,8 +10,8 @@ sh "$os/defaults.sh"
 sh "$os/binaries.sh"
 sh "$os/apps.sh"
 
-if [ -e "$os/$install_type/apps.sh" ]; then
-	sh "$os/$install_type/apps.sh"
+if [ -e "$os/$install/apps.sh" ]; then
+	sh "$os/$install/apps.sh"
 fi
 
 # Symlink the profile
